1010fe14fb247674c5369fe029b6e8b6c3e91f12
b-bots-dev <b.bots.dev@gmail.com>
2023-09-02 00:02:04 +0200

    menu - early beginning

---
+++ TG_BOT/keyboards.py
@@ -0,0 +1,27 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+def keyboards(keyboard, param=None):
+
+    if keyboard == 'menu':
+        btn1 = InlineKeyboardButton('MONITOR FREQUENCY', callback_data=f'freq_main.{param}')
+        btn2 = InlineKeyboardButton('empty', callback_data='xxx')
+        btn3 = InlineKeyboardButton('CLOSE', callback_data=f'close.{param}')
+        buttons = [btn1, btn2, btn3]
+        kb = InlineKeyboardMarkup([buttons])
+        return kb
+
+    if keyboard == 'freq_main':
+        btn1 = InlineKeyboardButton('CHANGE', callback_data=f'freq_set.{param}')
+        btn2 = InlineKeyboardButton('BACK', callback_data='menu')
+        btn3 = InlineKeyboardButton('CLOSE', callback_data=f'close.{param}')
+        buttons = [btn1, btn2, btn3]
+        kb = InlineKeyboardMarkup([buttons])
+        return kb
+
+    if keyboard == 'freq_back':
+        btn1 = InlineKeyboardButton('BACK', callback_data=f'freq_main.{param}')
+        btn2 = InlineKeyboardButton('CLOSE', callback_data=f'close.{param}')
+        buttons = [btn1, btn2]
+        kb = InlineKeyboardMarkup([buttons])
+        return kb
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
+import pyrostep
 import DATA.common_globals as cg
-from FUSI.Monitor import Monitor
 from pyrogram import Client, filters, idle
+from FUSI.Monitor import Monitor
 from COM.now_is import now_is
+from TG_BOT.keyboards import keyboards
 
 PATH = os.getcwd()
 
@@ -14,11 +16,78 @@ bot = Client("fusi-render",
              api_hash=os.environ['API_HASH'],
              bot_token=os.environ['BOT_TOKEN'])
 
+pyrostep.listen(bot)
+
 
 @bot.on_message(filters.command('start'))
 async def start_command(client, msg):
-    cid = msg.chat.id
+    await msg.reply('Alive')
 
+
+@bot.on_message(filters.command('menu'))
+async def menu_command(client, msg):
+    cid = msg.chat.id
+    uid = msg.from_user.id
+    await bot.delete_messages(cid, msg.id)
+    await bot.send_message(cid, 'MAIN MENU'
+                                '\n\nSelect option',
+                           reply_markup=keyboards('menu', uid))
+
+
+@bot.on_message(filters.command('freq'))
+async def freq_command(client, msg):
+    uid = msg.from_user.id
+    await msg.reply(f'Set monitor refresh frequency in seconds'
+                    f'\n\nCurrent frequency : {cg.refresh_freq}s',
+                    reply_markup=keyboards('freq_main', uid))
+
+
+@bot.on_callback_query()
+async def callback_query(client, call):
+    cid = call.message.chat.id
+    mid = call.message.id
+
+    # CLOSE
+    if call.data.startswith('close'):
+        uid = call.data.split('.')[1]
+        await pyrostep.unregister_steps(uid)
+        await bot.delete_messages(cid, mid)
+
+    # MENU
+    if call.data == 'menu':
+        await bot.edit_message_text(cid, mid, 'MAIN MENU'
+                                              '\n\nSelect option',
+                                    reply_markup=keyboards('menu'))
+
+    # FREQUENCY
+    if call.data.startswith('freq_main'):
+        uid = call.data.split('.')[1]
+        if uid is not None:
+            await pyrostep.unregister_steps(uid)
+        await bot.edit_message_text(cid, mid, f'Set monitor refresh frequency in seconds'
+                                              f'\n\nCurrent frequency : {cg.refresh_freq}s',
+                                    reply_markup=keyboards('freq_main', uid))
+
+    if call.data.startswith('freq_set'):
+        uid = call.data.split('.')[1]
+        print(call.data)
+        print(uid)
+        await bot.edit_message_text(cid, mid, f'Set monitor refresh frequency in seconds'
+                                              f'\n\nCurrent frequency : {cg.refresh_freq}s'
+                                              f'\n\nEnter value in seconds (min 1 - 60 max)',
+                                    reply_markup=keyboards('freq_back', uid))
+        await pyrostep.register_next_step(uid, freq_set)
+
+
+# STEP HANDLERS
+async def freq_set(client, msg):
+    print(msg.text)
+
+
+try:
+    bot.stop()
+except ConnectionError:
+    pass
 
 bot.start()
 monitor = Monitor(bot, PATH)
