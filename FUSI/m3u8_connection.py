6ec70ab983f603a164c9f6450d384f44af0d4a8c
b-bots-dev <b.bots.dev@gmail.com>
2023-09-11 09:10:38 +0200

    exception added

--- FUSI/m3u8_connection.py
+++ FUSI/m3u8_connection.py
@@ -8,12 +8,15 @@ def m3u8_test(bot, link):
         'User-Agent': 'Mozilla/5.0 (Linux; Android 7.1.2; ASUS_Z01QD) AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/86.0.4240.198 Mobile Safari/537.36'}
 
-    temp_link = link.split('?')
-    url = temp_link[0]
-    query = temp_link[1].split('&')
-    sign = query[0].split('=')
-    t = query[1].split('=')
-    querystring = {sign[0]: sign[1], t[0]: t[1]}
+    try:
+        temp_link = link.split('?')
+        url = temp_link[0]
+        query = temp_link[1].split('&')
+        sign = query[0].split('=')
+        t = query[1].split('=')
+        querystring = {sign[0]: sign[1], t[0]: t[1]}
+    except IndexError:
+        return False
 
     try:
         with requests.get(url, params=querystring, headers=headers) as response:
